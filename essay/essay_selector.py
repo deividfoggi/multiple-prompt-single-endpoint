bacb8ce3bec0e50bb4245907f89218743bf20792
deividfoggi <dev1@mngenvmcap380802.onmicrosoft.com>
2025-02-18 16:34:28 -0300

    feat: update essay selector to handle JSON input and integrate Azure Blob storage

--- essay/essay_selector.py
+++ essay/essay_selector.py
@@ -7,9 +7,7 @@ from promptflow.core import tool
 import os
 from promptflow.core import Prompty
 from promptflow.core import AzureOpenAIModelConfiguration
-# import blob storage
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
-from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient, BlobClient
 from dotenv import load_dotenv
 
 # The inputs section will change based on the arguments of the tool function, after you save the code
@@ -37,36 +35,36 @@ class Essay:
     def __init__(self, model_config: AzureOpenAIModelConfiguration):
         self.model_config = model_config
 
-    def __call__(self, essay: str) -> str:
+    def __call__(self, essay: str) -> json:
         # parse essay as a json
-        essay = json.loads(essay)
-        print("Essay type: " + essay['essay_type'])
-        prompt_file_name = essay["essay_type"] + ".prompty"
+        essay_json = json.loads(essay)
+        print("Essay type: " + essay_json['essay_type'])
+        prompt_file_name = essay_json["essay_type"] + ".prompty"
         blob_client = BlobClient(prompt_file_name)
         prompt_file = blob_client(blob_name=prompt_file_name)
         prompty = Prompty.load(source=prompt_file)
 
         # Create a dictionary of arguments
         prompty_args = {
-            "language": essay["language"],
-            "genre": essay["genre"],
-            "statement": essay["statement"],
-            "title": essay["title"],
-            "essay": essay["essay"],
-            "support_material": essay["support_text"],
-            "skills": essay["skills"]
+            "language": essay_json["language"],
+            "genre": essay_json["genre"],
+            "statement": essay_json["statement"],
+            "title": essay_json["title"],
+            "essay": essay_json["essay"],
+            "support_material": essay_json["support_text"],
+            "skills": essay_json["skills"]
         }
 
         result = prompty(**prompty_args)
-        
+
         return result
 
 @tool
-def essay_selector(essay: str) -> str:
+def essay_selector(essay: str) -> json:
     model_config = AzureOpenAIModelConfiguration(
             azure_deployment=os.getenv("AZURE_DEPLOYMENT")
         )
+    print(essay)
     essay_instance = Essay(model_config)
     result = essay_instance(essay)
     return result
-    
